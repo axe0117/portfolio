6e689036e94b810eec223d33e6b21215a55734ed
Axe but laptop <axe0117@gmail.com>
2026-07-13 15:09:36 -0400

    dark mode toggle

--- script.js
+++ script.js
@@ -8,3 +8,38 @@
 // 	console.log(coords);
 // });
 //https://www.youtube.com/watch?v=IKOwKiVt_x8&list=PL4FL-XepWXttYviyLh0yEiNOhAj_qCYs4&index=14
+
+const toggle = document.querySelector("#toggleButton");
+const toggleLabel = document.querySelector("#toggleLabel");
+
+toggle.addEventListener("change", toggleChanged);
+
+function toggleChanged() {
+    if (toggle.checked) {
+        document.body.classList.add("light-mode");
+        toggleLabel.innerText = "Light Mode";
+    } else {
+        document.body.classList.remove("light-mode");
+        toggleLabel.innerText = "Dark Mode";
+    }
+} 
+
+//carousel time woooooooo
+
+const imgs = [];
+
+const leftArrow = document.querySelector("#left-arrow");
+const rightArrow = document.querySelector("#right-arrow");
+const currentPhoto = document.querySelector("#current-photo");
+
+leftArrow.addEventListener("click", moveLeft);
+rightArrow.addEventListener("click", moveRight);
+let i = 0;
+
+function moveLeft() {
+    i = i === 0 ? imgs.length - 1 : i - 1;
+    currentPhoto.setAttribute("src", imgs[i]);
+}
+
+function moveRight() {
+    
