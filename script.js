c461f426ba2804548069554e3b80edb3ccb1ed71
Axe but laptop <axe0117@gmail.com>
2026-07-13 13:23:55 -0400

    old

---
+++ script.js
@@ -0,0 +1,8 @@
+//my attempt at trying to put a 3d model in a webpage bc three.js was too hard and
+//setup took way too long for just a simple model import
+
+//anyway I dug into game files using a software and had to convert the model file
+//into an fbx using blender so if this works,
+
+//Model made by Cygames
+
