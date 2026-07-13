807bb011d91ee9aa874c47fcccb5d8c351b20a96
axe0117 <axe0117@gmail.com>
2026-07-13 00:52:35 -0400

    added oguri yay

---
+++ script.js
@@ -0,0 +1,10 @@
+//cursor trail
+// const coords = { x: 0, y: 0 };
+
+// window.addEventListener("mousemove", function (e) {
+// 	coords.x = e.clientX;
+// 	coords.y = e.clientY;
+
+// 	console.log(coords);
+// });
+//https://www.youtube.com/watch?v=IKOwKiVt_x8&list=PL4FL-XepWXttYviyLh0yEiNOhAj_qCYs4&index=14
