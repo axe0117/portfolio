59a2578be7be8f44d9cf08af956a0f4534a3e08c
Axe but laptop <axe0117@gmail.com>
2026-07-21 15:39:12 -0400

    added: server

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const fs = require("fs");
+const port = 3000;
+
+const server = http.createServer();
+server.on("request", (req, res) => {
+    let url = req.url;
+
+    console.log(url);
+    if(url === "/") {
+        res.writeHead(200, {"Content-type": "text/html"});
+        fs.createReadStream("./index.html").pipe(res);
+    }
+    else if(url === "/exp") {
+        res.writeHead(200, {"Content-type": "text/html"});
+        fs.createReadStream("./exp.html").pipe(res);
+    }
+    else if(url === "/about") {
+        res.writeHead(200, {"Content-type": "text/html"});
+        fs.createReadStream("./about.html").pipe(res);
+    }
+    else if(url === "/project") {
+        res.writeHead(200, {"Content-type": "text/html"});
+        fs.createReadStream("./project.html").pipe(res);
+    }
+    else if(url === "/axetao.ico") {
+        res.writeHead(200, {"Content-type": "image/ico"});
+        fs.createReadStream("./axetao.ico").pipe(res);
+    }
+    else if(url === "/style.css") {
+        res.writeHead(200, {"Content-type": "text/css"});
+        fs.createReadStream("./style.css").pipe(res);
+    }
+    else if(url === "/script.js") {
+        res.writeHead(200, {"Content-type": "text/javascript"});
+        fs.createReadStream("./script.js").pipe(res);
+    }
+})
+
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
